24559bc53733e3cf8668a81306bdb44d9b5340bd
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2025-11-05 16:28:20 -0300

    - Criando o contexto e acesso ao banco de dados junto da string de conexão na camada API

---
+++ ProductCRUD.Data/Context/ProductContext.cs
@@ -0,0 +1,26 @@
+﻿using ProductCRUD.Domain.Entities;
+using System.Data.Entity;
+
+namespace ProductCRUD.Data.Context
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Preco)
+                .HasPrecision(18, 2);
+        }
+    }
+}
---
+++ ProductCRUD.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using ProductCRUD.Data.Context;
+using ProductCRUD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductCRUD.Data.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ProductContext _context;
+
+        public ProductRepository()
+        {
+            _context = new ProductContext();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products.OrderBy(p => p.Nome).ToList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _context.Products.Find(id);
+        }
+
+        public Product Create(Product product)
+        {
+            product.DataCadastro = DateTime.Now;
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return product;
+        }
+
+        public Product Update(Product product)
+        {
+            var existing = _context.Products.Find(product.Id);
+            if (existing != null)
+            {
+                existing.Nome = product.Nome;
+                existing.Descricao = product.Descricao;
+                existing.Preco = product.Preco;
+                _context.SaveChanges();
+            }
+            return existing;
+        }
+
+        public bool Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
